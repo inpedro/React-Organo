0c366b158be3929b04cda9bf170bbaf43a0ad50e
João Pedro Leandro Silva <tec.joao.leandro@mte.br>
2023-08-31 11:02:41 -0300

    React 1º versão de projetos

---
+++ src/componentes/Banners/index.js
@@ -0,0 +1,9 @@
+import logoBanner from '../img/imagens/banner.png';
+import './index.css';
+function Banners()  {
+    return (
+        <img src={logoBanner} className="logoBanner"alt="logotipo"></img>
+    )
+}
+
+export default Banners;
